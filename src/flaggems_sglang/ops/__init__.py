a8aaab81bbb37c65593a09926cfadccd3696f11b
yangy0906 <yangyang0906c@163.com>
2026-06-11 02:58:35 +0000

    style: fix isort and black formatting for CI

--- src/flaggems_sglang/ops/__init__.py
+++ src/flaggems_sglang/ops/__init__.py
@@ -1,10 +1,6 @@
 from flaggems_sglang.ops.gemma_rms_norm import gemma_rms_norm
-
 from flaggems_sglang.ops.mrotary_embedding import (  # noqa: F401
     mrotary_embedding,
 )
 
-__all__ = [
-  "gemma_rms_norm",
-  "mrotary_embedding"
-]
+__all__ = ["gemma_rms_norm", "mrotary_embedding"]
